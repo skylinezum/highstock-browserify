9e741731eed3e563ae6f2fbb52aeb4c72f4da1f1
Jimmy Chan <jimmychan@morrosystems.com>
2015-12-30 12:10:05 -0800

    update high stock version
    - update high stock version

--- servershim.js
+++ servershim.js
@@ -1,5 +1,5 @@
 // define browser objects so require doesnt blow up in tests
 
 var document = {createElement:function(name){ return {getContext:function(){}} },documentElement:{}};
-var navigator = {userAgent:'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.3 Safari/537.36'};
+var navigator = {userAgent:'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'};
 var window = {navigator:navigator};
